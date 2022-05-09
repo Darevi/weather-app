b79b8bd7b1cb43201a3c102925b873bca7034796
tarugaru <31079485+tarugaru@users.noreply.github.com>
2022-05-09 14:15:31 -0700

    update location screen dart
    getting 2 issues w dispose and initState-- will fix

--- lib/LocationScreen.dart
+++ lib/LocationScreen.dart
@@ -3,8 +3,24 @@
 import 'package:flutter/material.dart';
 
 // hello there
+//theoretically I think this is public and
+//can be accessed from anywhere in the app
+String locToSearch = " ";
 
 class LocationScreen extends StatelessWidget {
+  late TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,6 +50,30 @@ class LocationScreen extends StatelessWidget {
                         hintText: 'Search a Location',
                         prefixIcon: Icon(Icons.search),
                         suffixIcon: Icon(Icons.add_location)),
+                    controller: _controller,
+                    onSubmitted: (String value) async{
+                      locToSearch = value;
+                      await showDialog<void>(
+                        context: context,
+                        builder: (BuildContext context){
+                          return AlertDialog(
+                            title: const Text('Thanks!'),
+                            content: Text(
+                                'You are searching for this location: "$value"'
+                            ),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: (){
+                                  Navigator.pop(context);
+                                },
+                                child: const Text("Ok"),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+
+                    },
                   ),
                 ),
               ),
