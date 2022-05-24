73dacd3dcaebfdb9746df1693b2f7006476816ed
Darevi <joebart98@gmail.com>
2022-05-23 17:52:18 -0700

    comments

--- lib/jsonReader.dart
+++ lib/jsonReader.dart
@@ -3,14 +3,16 @@ import 'package:flutter/services.dart';
 import 'dart:convert';
 
 class JsonReader {
+  // lists that can be accessed publicly
   List<Location> locationList = <Location>[];
   List<String> locationStringList = <String>[];
 
   Future<void> readJson() async {
+    // turns JSON file to a string
     final String cityString = await rootBundle.loadString('assets/cities.json');
     final List list = json.decode(cityString) as List<dynamic>;
 
-
+    // adds locations in JSON array into locationList and turns location into a String for locationStringList
     for (int i = 0; i < list.length; i++) {
       final tempItem = list[i];
 
