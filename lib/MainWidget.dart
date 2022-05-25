35e0109789256d32220522e35184e8c8fe75ce56
tarugaru <31079485+tarugaru@users.noreply.github.com>
2022-05-25 14:44:18 -0700

    Update MainWidget.dart

--- lib/MainWidget.dart
+++ lib/MainWidget.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'tools/weather_tile.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_weather_bg_null_safety/flutter_weather_bg.dart';
 
 class MainWidget extends StatelessWidget {
   final location;
