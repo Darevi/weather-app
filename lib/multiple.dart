6079738c96f03299c67df094ab0019e2d4cb71e3
DavidLee36 <104027396+DavidLee36@users.noreply.github.com>
2022-05-27 20:11:57 -0700

    Combined Search and Multiple pages

--- lib/multiple.dart
+++ lib/multiple.dart
@@ -5,28 +5,29 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:mock_weather/MultiplesForecastItem.dart';
 import 'MultiplesForecastData.dart';
+import 'locations.dart';
 
 class Multiple extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _MultipleState();
 }
 
-class _MultipleState extends State<Multiple> {
+ class _MultipleState extends State<Multiple> {
   //List of location(s).
   List<Location> locs = [
     Location(
-        latitude: "47.608013", longitude: "-122.335167", cityName: "Seattle")
+        "Seattle", "", "", double.parse("47.608013"), double.parse("-122.335167"))
   ];
 
   //Method to iterate through the location list, which makes an API call for each location
   //..and adds each new MultiplesForecastData object to the list.
-  Future<List<MultiplesForecastData>> _getForecasts(
+  static Future<List<MultiplesForecastData>> _getForecasts(
       List<Location> locations) async {
     List<MultiplesForecastData> forecasts = [];
     for (Location l in locations) {
       const apiKey = "01787ca7c37221e8632a2dab11901f4c";
       final requestUrl =
-          "https://api.openweathermap.org/data/2.5/weather?lat=${l.latitude}&lon=${l.longitude}&units=imperial&appid=$apiKey";
+          "https://api.openweathermap.org/data/2.5/weather?lat=${l.lat}&lon=${l.lon}&units=imperial&appid=$apiKey";
       final response = await http.get(Uri.parse(requestUrl));
 
       if (response.statusCode == 200) {
@@ -80,7 +81,7 @@ class _MultipleState extends State<Multiple> {
 }
 
 //Dummy class for the locations.
-class Location {
+/*
   final String latitude;
   final String longitude;
   final String cityName;
@@ -89,4 +90,4 @@ class Location {
       {required this.latitude,
       required this.longitude,
       required this.cityName});
-}
+}*/
